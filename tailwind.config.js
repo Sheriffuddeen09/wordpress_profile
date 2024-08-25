7418b8587b3702861ff91aa360bff382d8161e5a
Sheriffuddeen20 <odukoyasheriff@gmail.com>
2024-08-25 23:40:21 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwind-scrollbar")],
-  variants: {
-    // ...
-    scrollbar: ['dark']
+  plugins:[]
+
 }
-}
+
 
