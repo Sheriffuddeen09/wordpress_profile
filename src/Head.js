5522c76456e8f121622e2a7bf9e2116e37129a36
Sheriffuddeen09 <odukoyasheriff@gmail.com>
2024-10-31 07:17:56 +0100

    commit

--- src/Head.js
+++ src/Head.js
@@ -16,7 +16,7 @@ const Head = () =>{
         
         <header className="bg-white">
             <section className=" header pb-1 px-6 pt-6 flex justify-between items-center ">
-            <div className="flex justify-between mx-3">
+            <div className="flex justify-between">
                 <h1 className="text-3xl font-bold text-blue-700 h1">SheriffPortfolio</h1>
                 <button onClick={handleToggle} 
                 className="text-3xl sm:hidden relative -top-2
